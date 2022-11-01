0613b86957cdf7f5fd687c97e1ed14c5c3275ccf
Ana Gallegos <agallegosgelabert@gmail.com>
2022-11-01 11:56:14 +0100

    Created entries and labels

--- Day 29 Password Manager GUI/main.py
+++ Day 29 Password Manager GUI/main.py
@@ -12,5 +12,23 @@ canvas = Canvas(width=200, height=200, highlightthickness=0)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
 canvas.pack()
+canvas.grid(row = 0, column = 1)
 
+# Labels
+website_label = Label(text = "Website:")
+website_label.grid(row = 1, column = 0)
+email_label = Label(text = "Email/Username:")
+email_label.grid(row = 2, column = 0)
+password_label = Label(text = "Password:")
+password_label.grid(row = 3, column = 0)
+
+# Entries
+website_entry = Entry(width = 35)
+website_entry.grid(row = 1, column= 1)
+
+email_entry = Entry(width = 35)
+email_entry.grid(row = 2, column= 1)
+
+password_entry = Entry(width = 35)
+password_entry.grid(row = 3, column= 1)
 window.mainloop()
