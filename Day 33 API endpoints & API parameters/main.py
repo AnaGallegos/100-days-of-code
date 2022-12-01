4c109c482c42207efa35841389bbfb9ea0737232
Ana Gallegos <anagallegos1995@gmail.com>
2022-12-01 14:53:34 +0100

    day 33 upload

--- Day 33 API endpoints & API parameters/main.py
+++ Day 33 API endpoints & API parameters/main.py
@@ -1,30 +1,58 @@
-from tkinter import *
 import requests
+from datetime import datetime
+import smtplib
 
 
 
-def get_quote():
-    response = requests.get("https://api.kanye.rest/")
+MY_LAT = 39.577261 # Your latitude
+MY_LONG = 2.627258 # Your longitude
+MY_EMAIL = "EXAMPLE@GMAIL.COM"
+MY_PASSWORD = "000000"
+
+''''IN ORDER TO KNOW IF WE CAN SEE THE ISS   
+WE ARE GOING TO MAKE AN APP THAT TELLS US:
+1. IF ITS OVER OUR HEAD (VISIBLE FOR OUR LOCATION)
+2. IF ITS NIGHT (YOU CAN'T SEE THE SATELITE DURING DAYTIME)
+3. IF BOTH OF THEM ARE TRUE, WE ARE GOING TO RECIVE AN EMAIL TELLING US SO'''
+
+def is_iss_overhead():
+    # API REQUEST
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
-    quote = data["quote"]
-    canvas.itemconfig(quote_text, text = quote)
 
+    # GET ISS SATELITE LONG AND LAT
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+    # CHECKING IF IT'S OVER OUR HEAD
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Click my face!", width=250, font=("Arial", 15, "bold"), fill="white")
-canvas.grid(row=0, column=0)
+def is_night():
+    # WE GET THE SUNRISE AND SUNSET TIME FOR OUR LOCATION   
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
+    time_now = datetime.now().hour
 
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
-
-window.mainloop()
+if is_iss_overhead and is_night:
+    connection = smtplib.SMPT("smpt.gmail.com")
+    connection.starttls()
+    connection.login(MY_EMAIL, MY_PASSWORD)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs=MY_EMAIL,
+        msg="Subject:Look Up👆\n\nThe ISS is above you in the sky."
+    )
